656fdf443865766d65684cb274ae7beff90cb4c4
David ECKERT <david.ckrt@gmail.com>
2018-08-31 14:55:05 +0200

    implement module 5

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -21,7 +21,6 @@ class QuizQuestion extends Component {
               this.props.quiz_question.answer_options.map( (answer_option, index) => {
                 return <QuizQuestionButton key={index} button_text={answer_option}
                   clickHandler={this.handeClick.bind(this)} />
-                  "TOTO"
               })
             }
           </ul>
